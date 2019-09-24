6e2dca7a7e187c8403627102cc4aa48e0bc00d5d
Elmer Yu <ak64th@gmail.com>
2019-09-24 12:04:57 +0800

    feat(crawler): add `back_to` parameter

--- yby_fc/apps/crawler/spiders/bilibili.py
+++ yby_fc/apps/crawler/spiders/bilibili.py
@@ -1,4 +1,7 @@
+import datetime
+
 import bs4
+import dateutil.parser
 import scrapy
 
 
@@ -11,11 +14,24 @@ class BilibiliSpider(scrapy.Spider):
         },
     }
 
+    def __init__(self, back_to=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if back_to is not None:
+            if isinstance(back_to, str):
+                back_to = dateutil.parser.parse(back_to)
+            elif not isinstance(back_to, datetime.date):
+                raise ValueError(
+                    'The `back_to` parameter should be a `datetime.date`',
+                    ' object or string',
+                )
+        self.back_to = back_to
+
     def start_requests(self):
         yield scrapy.Request(
             url='https://search.bilibili.com/all?keyword=杨冰怡&order=pubdate',
         )
 
     def parse(self, response):
-        soup = bs4.BeautifulSoup(response.text)
-        print(soup.prettify())
+        soup = bs4.BeautifulSoup(response.text, features='lxml')
+        print(self.back_to)
+        print(soup.title.get_text())
--- yby_fc/apps/crawler/tasks.py
+++ yby_fc/apps/crawler/tasks.py
@@ -6,8 +6,8 @@ from yby_fc.apps.crawler.spiders.bilibili import BilibiliSpider
 
 
 @shared_task()
-def crawl():
+def crawl(back_to=None):
     crawler_settings = getattr(settings, 'CRAWLER_SETTINGS', None)
     process = CrawlerProcess(settings=crawler_settings)
-    process.crawl(BilibiliSpider)
+    process.crawl(BilibiliSpider, back_to=back_to)
     process.start()
